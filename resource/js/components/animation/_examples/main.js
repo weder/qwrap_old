da19940a9dae7da17690b344b0e5f51a9ee9a622
akira-cn <akira.cn@gmail.com>
2010-09-14 17:40:28 +0800

    close

--- resource/js/components/animation/_examples/main.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	var srcPath='../../../';
-
-	document.write('<script type="text/javascript" src="' +srcPath+ 'apps/core_dom_all.js"><\/script>');
-	document.write('<script type="text/javascript" src="' +srcPath+ 'components/animation/anim.js"></script>');
-})();
